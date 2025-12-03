fcc4226bae129a114319aa8a9a33c29139706b10
Vikrant Bhurke <bhurkevi@gmail.com>
2025-12-03 22:41:09 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: [
+      "@mantine/core",
+      "@mantine/hooks",
+      "@mantine/carousel",
+      "@mantine/charts",
+      "@mantine/dates",
+      "@mantine/dropzone",
+      "@mantine/form",
+      "@mantine/modals",
+      "@mantine/notifications",
+      "@mantine/nprogress",
+      "@mantine/tiptap",
+    ],
+  },
 };
 
 export default nextConfig;
